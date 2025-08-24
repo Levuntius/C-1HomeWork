f2951744432184b5205a3643a5240fd6941a4699
Leo <levgladishev@icloud.com>
2025-08-24 18:02:54 +0100

    Add project files.

---
+++ C#1HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_1HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
